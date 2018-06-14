a1eae80eb6e64fe6466a15cdd6796eb73cdc01ec
Sidney Widmer <sidneywidmer@gmail.com>
2018-06-14 14:21:26 +0200

    Refactor DjangocmsVueJsMixin to allow for static url generation
    Since we don't want to print hundreds of detail model urls in the frontend, we can generate routes à la
    en/api/model/:model in the menu_helpers of a model. The current implementation doesn't allow
    this and needs some nasty hacks sind this needs to be done with static methods. This refactor
    introduces said methods.

--- djangocms_spa_vue_js/models.py
+++ djangocms_spa_vue_js/models.py
@@ -1,5 +1,10 @@
 from appconf import AppConf
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import Model
+from django.urls import reverse
+from django.utils.translation import override, get_language
 from django.views.defaults import ERROR_404_TEMPLATE_NAME
+
 from djangocms_spa.content_helpers import get_frontend_data_dict_for_placeholders, get_global_placeholder_data
 from djangocms_spa.models import DjangoCmsMixin
 
@@ -11,32 +16,89 @@ class DjangoCmsSPAVueJSConf(AppConf):
 
 class DjangocmsVueJsMixin(DjangoCmsMixin):
     """
-    This mixin prepares the data of a model to be ready for the frontend.
+    Set up some helper methods to prepare the model for the vue frontend integration.
+    This mixin defines the different routes and sets up functions to generate the
+    json output for either list or detail view.
     """
-    vue_js_router_component = 'topic-detail'
+
+    # Configs used to generate vue specific stuff
+    vue_config = {
+        'router_name': 'app-model',
+        'router_component': 'model-detail',
+        'route_pattern': ':model',
+    }
+
+    # Config dict holding the route groups indexed by group name. Each group contains different
+    # view routes, normally there is one for the list view and one for the detail view.
+    routes = {
+        'api': {'list': 'model_list_api', 'detail': 'model_detail_api'},
+        'normal': {'list': 'model_list', 'detail': 'model_detail'},
+    }
+
+    # Kwargs that need no be passed to the reverse() function to resolve a specific named route.
+    # We assume that views in different groups all need the same route parameters.
+    #
+    # Possible options:
+    #   - None                          -> no kwargs needed
+    #   - 'string'                      -> string, meaning that your route parameter and your model field share
+    #                                      the same name
+    #   - ('parameter', 'attribute')    -> tuple, your model route parameter and field name are different
+    #   - ['string', 'string']          -> list with strings, tuples or a mix of both
+    route_parameters = {
+        'list': None,
+        'detail': 'slug',
+    }
 
     class Meta:
         abstract = True
 
     @property
-    def vue_js_router_name(self):
-        return '%s-%s' % (self._meta.app_label, self._meta.model_name)
+    def vue_js_link(self) -> dict:
+        """ Generate detail api view url so the vue router knows where to fetch this model """
+        return {
+            'name': self.vue_config['router_name'],
+            'fetch': self.url(group='api', view='detail')
+        }
 
-    def get_frontend_list_data_dict(self, request, editable=False, placeholder_name=''):
+    @classmethod
+    def menu_node_attributes(cls) -> dict:
+        # TODO: parameter for group and view
+        """ Get additional attributes for custom NavigationNode """
+        return {
+            'component': cls.vue_config['router_component'],
+            'vue_js_router_name': cls.vue_config['router_name'],
+            'named_route_path_pattern': cls.vue_config['route_pattern'],
+            'named_route_path': cls._vue_pattern_url(group='api', view='detail'),
+            'fetch_url': cls._vue_pattern_url(group='normal', view='detail'),
+        }
+
+    def get_frontend_list_data_dict(
+        self,
+        request: WSGIRequest,
+        editable: bool = False,
+        placeholder_name: str = ''
+    ) -> dict:
+        """
+        Prepare a base dict for the list view. This should be extended by the model
+        to include any additional data. If no custom view is defined in
+        views.py this method is not used.
+        """
         data = {}
 
         if editable:
             data.update(self.get_cms_placeholder_json(request=request, placeholder_name=placeholder_name))
 
         data.update({
-            'content': {
-                'id': self.pk,
-                'link': self.get_vue_js_link_dict(),
-            }
+            'content': {'id': self.pk, 'link': self.vue_js_link}
         })
+
         return data
 
-    def get_frontend_detail_data_dict(self, request, editable=False):
+    def get_frontend_detail_data_dict(self, request: WSGIRequest, editable: bool = False) -> dict:
+        """
+        Same as get_frontend_list_data_dict but for the model detail view. Also takes care of any
+        PlaceholderField on the model and renders their plugins.
+        """
         data = {}
 
         # Add all placeholder fields.
@@ -55,40 +117,74 @@ class DjangocmsVueJsMixin(DjangoCmsMixin):
 
         return data
 
-    def get_vue_js_link_dict(self):
-        return {
-            'name': self.vue_js_router_name,
-            'fetch': self.get_api_detail_url()
-        }
+    def url(self, group: str, view: str, language: str = None) -> str:
+        """
+        Acts as a proxy to _static_url with less arguments since this method
+        will be called on a model instance and is not static.
+        """
+        language = language or get_language()
 
-    def get_cms_menu_node_attributes(self):
-        return {
-            'component': self.get_detail_view_component(),
-            'vue_js_router_name': self.vue_js_router_name,
-            'absolute_url': self.get_absolute_url(),
-            'fetch_url': self.get_api_detail_url(),
-            'named_route_path_pattern': self.get_detail_path_pattern(),
-            'url_params': self.get_url_params(),
-        }
+        return self._static_url(group, view, self, language)
 
-    def get_absolute_url(self):
-        # Override this method in your model.
-        return ''
+    @classmethod
+    def _static_url(
+        cls,
+        group: str,
+        view: str,
+        model: object = None,
+        language: str = None
+    ) -> str:
+        """
+        Generate reverse() url to route view based on the passed in 'routes' dict. cls.route_parameters and 'model'
+        is then used to generate the correct kwargs parameters for reverse().
+        """
+        language = language or get_language()
 
-    def get_api_detail_url(self):
-        # Override this method in your model.
-        return ''
+        route_name = cls.routes[group][view]
 
-    def get_detail_view_component(self):
-        # Override this method in your model.
-        return ''
+        with override(language):
+            return reverse(route_name, kwargs=cls._reverse_kwargs(cls.route_parameters[view], model))
 
-    def get_detail_path_pattern(self):
-        # Used to group routes (dynamic route matching). Override this method in your model.
-        return ':slug'
+    @classmethod
+    def _vue_pattern_url(
+        cls,
+        group: str = 'normal',
+        view: str = 'detail',
+        valid: bool = False
+    ) -> str:
+        """
+        Since we don't want to print hundreds of detail model urls in the frontend, we can generate routes à la
+        en/api/model/:model A NavigationNode needs a required url parameter and ':model' would not match
+        the regex defined in the model route. If we pass in valid = True a string like
+        'en/api/model/_replace_me_' will be returned. Since this NavigationNode is
+        only used for the frontend this is an "acceptable hack"™ :)
+        """
+        replace_string = '_replace_me_'
+        fakemodel = type('obj', (object,), {'slug': replace_string})
+        url = cls._static_url(group=group, view=view, model=fakemodel)
 
-    def get_url_params(self):
-        # Override this method in your model.
-        return {
-            'slug': ''
-        }
+        if valid:
+            return url
+
+        return url.replace(replace_string, cls.vue_config['route_pattern'])
+
+    @classmethod
+    def _reverse_kwargs(cls, route_attributes, model: object = None) -> dict:
+        return_kwargs = {}
+
+        if not isinstance(route_attributes, list):
+            route_attributes = [route_attributes]
+
+        for route_attribute in route_attributes:
+            if isinstance(route_attribute, str):
+                parameter = attribute = route_attribute
+            else:
+                parameter = route_attribute[0]
+                attribute = route_attribute[1]
+
+            return_kwargs[parameter] = getattr(model, attribute)
+
+        return return_kwargs
+
+    def get_absolute_url(self, language: str = None) -> str:
+        return self.url(group='normal', view='detail', language=language)
