402838428e5049a8020220a395db8bc045779225
Christian Schürmann <christian.schuermann@bluewin.ch>
2018-08-10 16:08:29 +0200

    Use dynamic JSON encoder

--- djangocms_spa_vue_js/templatetags/router_tags.py
+++ djangocms_spa_vue_js/templatetags/router_tags.py
@@ -1,6 +1,7 @@
 import json
 
 from django import template
+from django.conf import settings
 from django.utils.safestring import mark_safe
 
 from ..menu_helpers import get_vue_js_router
@@ -15,5 +16,5 @@ def vue_js_router(context):
     else:
         router = get_vue_js_router(context=context)
 
-    router_json = json.dumps(router)
+    router_json = json.dumps(router, cls=settings.DJANGOCMS_SPA_JSON_ENCODER)
     return mark_safe(router_json)
